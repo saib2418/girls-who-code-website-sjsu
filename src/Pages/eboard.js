8de2bac8de5da2c3ff8088528b477d2cc232ba09
Manasa <saib2418@gmail.com>
2022-08-17 19:49:50 -0700

    e-board page

--- src/Pages/eboard.js
+++ src/Pages/eboard.js
@@ -6,14 +6,14 @@ function Eboard() {
         <div className="content">
             <h1 className="content-header" id='eboard'>E-Board</h1>
             <div class="container">
-                <div class="col"><p>1</p></div>
-                <div class="col"><p>2</p></div>
-                <div class="col"><p>3</p></div>
-                <div class="col"><p>4</p></div>
-                <div class="col"><p>5</p></div>
-                <div class="col"><p>6</p></div>
-                <div class="col"><p>7</p></div>
-                <div class="col"><p>8</p></div>
+                <div class="col"><p> Sai Bharathula <br></br> President</p></div>
+                <div class="col"><p> Manogya Bharathula <br></br> External Vice President</p></div>
+                <div class="col"><p> Pushpal Patil <br></br> Internal Vice President</p></div>
+                <div class="col"><p> Yasesvi Punnaganti <br></br> Secretary</p></div>
+                <div class="col"><p> Taylor Lem <br></br> Treasurer</p></div>
+                <div class="col"><p> Fernanda Palcios <br></br>Event Planning Director</p></div>
+                <div class="col"><p>  Mrudhvika Sirineni <br></br> Social Media Director</p></div>
+                <div class="col"><p> Mikki Blue<br></br> Photographer</p></div>
 
             </div>
         </div>
